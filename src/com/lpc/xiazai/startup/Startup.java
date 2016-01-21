bd32c806c5e94730e0d1575a011ed1eddeb1d3d8
pengchenglpc <pengchenglpc@gmail.com>
2016-01-21 10:47:56 +0800

    重新定义类

--- src/com/lpc/xiazai/startup/Startup.java
+++ src/com/lpc/xiazai/startup/Startup.java
@@ -3,12 +3,12 @@ package com.lpc.xiazai.startup;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import com.lpc.xiazai.download.Download;
+import com.lpc.xiazai.download.DownloadRunnable;
 
 public class Startup {
 
 	public static void main(String[] args) throws MalformedURLException {
-		Download down = new Download();
+		DownloadRunnable down = new DownloadRunnable();
 		down.set(new URL("http://download.java.net/jdk/jdk-api-localizations/jdk-api-zh-cn/publish/1.6.0/chm/JDK_API_1_6_zh_CN.CHM"), "F:\\work\\logs");
 		Long time = System.currentTimeMillis();
 		down.run();
