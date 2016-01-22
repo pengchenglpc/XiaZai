b9b2f8f559a9907b7689f53929e12d332b853480
pengchenglpc <pengchenglpc@gmail.com>
2016-01-22 17:49:06 +0800

    上下文VO

---
+++ src/com/lpc/xiazai/vo/XiaZaiContextVo.java
@@ -0,0 +1,26 @@
+package com.lpc.xiazai.vo;
+
+public class XiaZaiContextVo {
+	private String id;
+	private String url;
+	private String tmpPath;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getTmpPath() {
+		return tmpPath;
+	}
+	public void setTmpPath(String tmpPath) {
+		this.tmpPath = tmpPath;
+	}
+	
+}
